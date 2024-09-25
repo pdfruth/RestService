37a382c2988e3e4570feb9698f7fff5fe7730788
pdfruth <pdfruth@gmail.com>
2024-09-24 23:48:01 -0600

    Add readiness check endpoint

---
+++ RestService/src/main/java/com/ibm/demo/RestService/SystemReadinessCheck.java
@@ -0,0 +1,20 @@
+package com.ibm.demo.RestService;
+
+import javax.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+@Readiness  // exposes http://<hostname>:<port>/health/ready endpoint
+@ApplicationScoped
+public class SystemReadinessCheck implements HealthCheck {
+	  private static final String READINESS_CHECK = "Readiness Check";
+	  
+	  @Override
+	  public HealthCheckResponse call() {
+		  	if (!System.getProperty("wlp.server.name").equals("RestServiceServer")) {
+		  		return HealthCheckResponse.down(READINESS_CHECK);
+		  	}
+		  	return HealthCheckResponse.up(READINESS_CHECK);
+	  }
+}
