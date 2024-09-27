fe8fb25746b6c53256648408d793b6f2f4ee5f13
pdfruth <pdfruth@gmail.com>
2024-09-26 18:07:56 -0600

    update comments

--- RestService/src/main/java/com/ibm/demo/RestService/HelloServlet.java
+++ RestService/src/main/java/com/ibm/demo/RestService/HelloServlet.java
@@ -15,16 +15,6 @@
  *******************************************************************************/
 package com.ibm.demo.RestService;
 
-/*
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-*/
-
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
